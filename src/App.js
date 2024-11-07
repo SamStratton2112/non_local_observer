50893e1db9c72b8122605bd3b67b9218d2c8e4e3
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-11-06 19:38:20 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Zoe's Online Store 
+    </div>
+  );
+}
+
+export default App;
