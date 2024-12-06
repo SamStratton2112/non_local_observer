19ec40b010175bd925b5169c520f85336b567b11
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-12-05 20:58:14 -0800

    Update NavBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -11,17 +11,17 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 const NavigationBar = () => {
   return (
     <Navbar collapseOnSelect expand="md" className="NavBar navbar-static-top NavBarBckground" data-bs-theme="dark">
-      <Navbar.Brand as={Link} to="/" className='nav-name NavBar-link'>
+      <Navbar.Brand as={Link} to="/" className='outlined nav-name NavBar-link'>
         Non Local Observer
       </Navbar.Brand>
       <Nav className="ms-auto">
         <Navbar.Toggle aria-controls="responsive-navbar-nav me-2"/>
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav.Link className='NavBar-link'  as={Link} to="/shop">Shop</Nav.Link>
-          <Nav.Link className='NavBar-link'  as={Link} to="/cart">Cart</Nav.Link>
-          <Nav.Link className='NavBar-link' as={Link} to="/contact">Contact</Nav.Link>
-          <Nav.Link className='NavBar-link'  as={Link} to="/">About Zoe</Nav.Link>
-          <Nav.Link className='NavBar-link social' as={Link} to="https://www.instagram.com/djmikewhite_/"><FontAwesomeIcon icon={faInstagram} size="2x" /></Nav.Link>
+          <Nav.Link className='NavBar-link outlined'  as={Link} to="/shop">Shop</Nav.Link>
+          <Nav.Link className='NavBar-link outlined'  as={Link} to="/cart">Cart</Nav.Link>
+          <Nav.Link className='NavBar-link outlined' as={Link} to="/contact">Contact</Nav.Link>
+          <Nav.Link className='NavBar-link outlined'  as={Link} to="/">About Zoe</Nav.Link>
+          <Nav.Link className='NavBar-link social' as={Link} to="https://www.instagram.com/djmikewhite_/"><FontAwesomeIcon icon={faInstagram} size="2x"/></Nav.Link>
         </Navbar.Collapse>
       </Nav>
     </Navbar>
